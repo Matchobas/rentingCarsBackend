50a1eed037449e284674c133a227888183ce8330
Matchobas <maalexandria@hotmail.com>
2022-06-10 23:23:16 -0300

    First jest test implemented and passing

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,32 @@
+import ICreateCategoryDTO from '../../DTOs/ICreateCategoryDTO';
+import Category from '../../entities/Category';
+import ICategoryRepository from '../ICategoriesRepository';
+
+class CategoriesRepositoriesInMemory implements ICategoryRepository {
+  categories: Category[] = [];
+
+  async findByName(name: string): Promise<Category> {
+    const category = this.categories.find((category) => category.name === name);
+
+    return category;
+  }
+
+  async all(): Promise<Category[]> {
+    return this.categories;
+  }
+
+  async create({ name, description }: ICreateCategoryDTO): Promise<Category> {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+    });
+
+    this.categories.push(category);
+
+    return category;
+  }
+}
+
+export { CategoriesRepositoriesInMemory };
--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,8 +1,24 @@
-describe('Create Category', () => {
-  it('should be able to create a category', () => {
-    const soma = 2 + 2;
-    const resultado = 4;
+import Category from '../../entities/Category';
+import { CategoriesRepositoriesInMemory } from '../../repositories/in-memory/CategoriesRepositoryInMemory';
+import CreateCategoryUseCase from './CreateCategoryUseCase';
 
-    expect(soma).toBe(resultado);
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoriesInMemory;
+
+describe('Create Category', () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoriesInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryInMemory);
+  });
+
+  it('should be able to create a new category', async () => {
+    const category = await createCategoryUseCase.execute({
+      name: 'Matheus',
+      description: 'Category description Test',
+    });
+
+    expect(category).toHaveProperty('name', 'Matheus');
+    expect(category).toHaveProperty('description', 'Category description Test');
+    expect(category).toBeInstanceOf(Category);
   });
 });
