84c1b14fb249b265a27ec2f89acfef0020cf0fe4
Matchobas <maalexandria@hotmail.com>
2022-06-25 00:56:24 -0300

    Not able to create car with duplicate plate

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+import { AppError } from '@shared/errors/AppError';
 
 import { CreateCarUseCase } from './CreateCarUseCase';
 
@@ -21,5 +22,29 @@ describe('Create Car', () => {
       brand: 'Ferrari',
       category_id: '12345678',
     });
+  });
+
+  it('should not be able to create a car with existing license plate', () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: 'Ferrari Enzo',
+        description: 'Red and really fast car',
+        daily_rate: 1500,
+        license_plate: 'AAA-6668',
+        fine_amount: 6000,
+        brand: 'Ferrari',
+        category_id: '12345678',
+      });
+
+      await createCarUseCase.execute({
+        name: 'Ferrari Enzo',
+        description: 'Red and really fast car',
+        daily_rate: 1500,
+        license_plate: 'AAA-6668',
+        fine_amount: 6000,
+        brand: 'Ferrari',
+        category_id: '12345678',
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
