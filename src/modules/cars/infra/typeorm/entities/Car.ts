2aa53bc449bea5a5edb9bea91ea2832cb945e1bd
Matchobas <maalexandria@hotmail.com>
2022-06-28 21:32:39 -0300

    Car entitie created with typeORM decorators

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,35 +1,49 @@
-import { v4 as uuidv4 } from 'uuid';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from 'typeorm';
 
+import Category from './Category';
+
+@Entity('cars')
 class Car {
+  @PrimaryColumn()
   id: string;
 
+  @Column()
   name: string;
 
+  @Column()
   description: string;
 
+  @Column()
   daily_rate: number;
 
-  available: boolean;
+  @Column()
+  available = true;
 
+  @Column()
   license_plate: string;
 
+  @Column()
   fine_amount: number;
 
+  @Column()
   brand: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @Column()
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
-
-  constructor() {
-    if (!this.available) {
-      this.available = true;
-    }
-
-    if (!this.id) {
-      this.id = uuidv4();
-    }
-  }
 }
 
 export { Car };
