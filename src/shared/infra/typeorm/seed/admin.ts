116ea309ce5d1f9a400caabdde65653f9bde89ae
Matchobas <maalexandria@hotmail.com>
2022-07-02 20:07:49 -0300

    Final fixes in admin creation command

--- src/shared/infra/typeorm/seed/admin.ts
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -10,10 +10,12 @@ async function create() {
   const password = await hash('admin', 8);
 
   await connection.query(
-    `INSERT INTO USERS(id, name, email, password, admin, created_at)
-      values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, ${new Date()})
+    `INSERT INTO USERS(id, name, email, password, is_admin, created_at, driver_license)
+      values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXXX')
     `
   );
+
+  connection.close();
 }
 
 create().then(() => console.log('User admin created!'));
